2b7815ee9a67bb1b370d148877b04a968c0f0c80
Arya Suwandika <thethirdswan@gmail.com>
2026-06-15 12:32:36 +0800

    1.1.1 for 1.21.1, fixed the same bug from other versions

--- src/main/java/com/thethirdswan/tfc_food_compat/mixin/MixinCreateBasinRecipeCompat.java
+++ src/main/java/com/thethirdswan/tfc_food_compat/mixin/MixinCreateBasinRecipeCompat.java
@@ -20,9 +20,9 @@ import java.util.Collection;
 import java.util.List;
 
 @Mixin(BasinRecipe.class)
-public class MixinCreateMixingCompat {
+public class MixinCreateBasinRecipeCompat {
     @Unique
-    private static ItemStack tfc_food_process_compat$input;
+    private static ItemStack tfc_food_process_compat$input = ItemStack.EMPTY;
 
     @Inject(method = "apply(Lcom/simibubi/create/content/processing/basin/BasinBlockEntity;Lnet/minecraft/world/item/crafting/Recipe;Z)Z", at = @At(value = "HEAD"), remap = false)
     private static void getItemInput(BasinBlockEntity basin, Recipe<?> recipe, boolean test, CallbackInfoReturnable<Boolean> cir) {
@@ -35,19 +35,19 @@ public class MixinCreateMixingCompat {
         }
     }
 //    does the singular add needs to be modified too?
-//    @ModifyArg(method = "apply(Lcom/simibubi/create/content/processing/basin/BasinBlockEntity;Lnet/minecraft/world/item/crafting/Recipe;Z)Z", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), remap = false)
-//    private static Object modifyAdd(Object o) {
-//        TFCFoodCompat.LOGGER.info("is the modified arg getting called?");
-//        if (o instanceof ItemStack) {
-//            TFCFoodCompat.LOGGER.info("what's the input: {}", tfc_food_process_compat$input);
-//            return ItemStackProvider.of((ItemStack) o, CopyFoodModifier.INSTANCE).getSingleStack(tfc_food_process_compat$input);
-//        }
-//        return o;
-//    }
+    @ModifyArg(method = "apply(Lcom/simibubi/create/content/processing/basin/BasinBlockEntity;Lnet/minecraft/world/item/crafting/Recipe;Z)Z", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), remap = false)
+    private static Object modifyAdd(Object o) {
+        if (tfc_food_process_compat$input.isEmpty()) return o;
+        if (o instanceof ItemStack) {
+            return ItemStackProvider.of((ItemStack) o, CopyFoodModifier.INSTANCE).getSingleStack(tfc_food_process_compat$input);
+        }
+        return o;
+    }
 
     @ModifyArg(method = "apply(Lcom/simibubi/create/content/processing/basin/BasinBlockEntity;Lnet/minecraft/world/item/crafting/Recipe;Z)Z", at = @At(value = "INVOKE", target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z"), remap = false)
     private static Collection<ItemStack> modifyAddAll(Collection<ItemStack> c) {
         List<ItemStack> newList = new ArrayList<>();
+        if (tfc_food_process_compat$input.isEmpty()) return c;
         c.forEach(item -> newList.add(ItemStackProvider.of(item, CopyFoodModifier.INSTANCE).getSingleStack(tfc_food_process_compat$input)));
         return newList;
     }
